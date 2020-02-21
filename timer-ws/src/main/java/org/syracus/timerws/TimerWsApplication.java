98e2a8e1a4c6a7d0502a3fbe6e6671955ac5664f
Sebastian Wiemer <sebastian.wiemer@datev.de>
2020-02-21 12:14:14 +0100

    adds timer-ws module

---
+++ timer-ws/src/main/java/org/syracus/timerws/TimerWsApplication.java
@@ -0,0 +1,13 @@
+package org.syracus.timerws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerWsApplication.class, args);
+    }
+
+}
---
+++ timer-ws/src/test/java/org/syracus/timerws/TimerWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syracus.timerws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
