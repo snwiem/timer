17cf2b7064e3269c94c8789240e554847e327c4b
Sebastian Wiemer <sebastian.wiemer@datev.de>
2020-02-24 08:56:17 +0100

    basic skelleton with bootstrap/jquery working

---
+++ timer-fe/src/main/java/org/syracus/timerfe/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.syracus.timerfe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
